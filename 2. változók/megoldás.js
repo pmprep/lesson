a26294f21fc9274ccd825fe5b6ce181078357e26
Andris <undris01@gmail.com>
2025-03-30 11:24:14 +0200

    feladatok

---
+++ 2. változók/megoldás.js
@@ -0,0 +1,96 @@
+// 1. Írd ki a console-ra hogy "hello world"
+console.log("hello world");
+alert("hello word");
+// 1. Írd ki a console-ra hogy 5
+console.log(5);
+alert(5);
+// 1. Írd ki a console-ra hogy true
+console.log(true);
+alert(true);
+// 1. Írd ki a console-ra hogy `A nevem John Conor, és szeretem Arnold Schwarzeneggert`
+console.log("A nevem John Conor, és szeretem Arnold Schwarzeneggert");
+alert("A nevem John Conor, és szeretem Arnold Schwarzeneggert");
+// 1. Írd ki a console-ra hogy `A terminátor széria első filmje: 1984-ben jelent meg`
+console.log("A terminátor széria első filmje: 1984-ben jelent meg");
+alert("A terminátor széria első filmje: 1984-ben jelent meg");
+// 1. Írd ki a console-ra hogy ` "Hasta la vista, baby!" - T800 101-es modell`
+console.log('Hasta la vista, baby!" - T800 101-es modell');
+alert('Hasta la vista, baby!" - T800 101-es modell');
+// 1. Írd ki a console-ra hogy  Arnold Schwarzeneggert a terminátor: - true
+console.log("Arnold Schwarzeneggert a terminátor:", true);
+
+alert("Arnold Schwarzeneggert a terminátor:" + true);
+alert("Arnold Schwarzeneggert a terminátor: true");
+
+alert("Arnold Schwarzeneggert a terminátor:");
+alert(true);
+// ## Felugró ablakba írás
+
+// 1. Az előző feladatokban szereplőeket írd ki felugró ablakba
+
+// ## Változók
+
+// 1. Készíts egy változót majd írd ki a tartalmát console-ra
+let var1 = true;
+console.log(var1);
+// 1. Készíts egy változót majd írd ki felugró ablakba a tartalmát
+alert(var1);
+// 1. Készíts 2 változót, írj bele számokat, írd ki az eredményét a console-ra
+let number1;
+number1 = 5;
+console.log(number1)
+number1 = 8;
+let number2;
+number2 = 7;
+console.log("A két szám összege", number1 + number2);
+
+let sum = number1 + number2;
+console.log(sum);
+// 1. Az előző feladat alapján készítsd el a feladatot, annyi különbséggel hogy a console-ra írd ki a két számot is majd jelezd hogy összeadod (x + y = eredmeny)
+console.log("Az első szám", number1);
+console.log("Az második szám", number2);
+console.log("Az összeg", number1, "+", number2, "=", sum);
+
+// 1. Hozz létre 3 változót, az első 2-be rakj számot, a harmadikba add össze őket, és írd ki az eredményt a console-ra.
+
+// 1. Hozz létre 2 változót, adj az első értéknek egy számot, a másodiknak egy szöveget. Add össze őket a console-ra
+
+// 1. Hozz létre 2 változót, az első értéke legyen egy szám, a másodiknak meg egy boolean érték, add össze őket és írd ki a console-ra az eredményt.
+console.log(10 + false)
+// 1. Írd ki a konzolra a számot 10.
+
+// 1. Írd ki a konzolra a neved.
+
+// 1. Készíts egy változót, amely tartalmazza a "Hello, World!" szöveget, majd írd ki a konzolra.
+
+// 1. Készíts egy változót, amely tartalmazza a korodat, majd írd ki a konzolra.
+
+// 1. Készíts egy változót, amely tartalmazza a kedvenc számodat, majd írd ki a konzolra.
+
+// 1. Írd ki a konzolra a számot 42.
+
+// 1. Írd ki a konzolra a "Hello, JavaScript!" szöveget.
+
+// 1. Készíts egy változót, amely tartalmazza a kedvenc színét, majd írd ki a konzolra.
+
+// 1. Készíts egy változót, amely tartalmazza a jelenlegi hónapot, majd írd ki a konzolra.
+
+// ## Bekérés
+
+// 1. Kérj be a felhasználótól 1 általad választott adatot, majd írasd ki a console-ra
+
+// 1. Kérj be a felhasználótól 1 általad választott adatot, majd írasd ki felugró ablakba
+
+// 1. Kérj be 2 szót a felhasználótól és consolera írasd ki egyszerre a kettőt
+
+// 1. Kérj be 2 számot a felhasználótól és console-ra írasd ki a 2 szám összegét
+
+// 1. Kérj be 2 számot a felhasználótól és console-ra írasd ki a 2 szám különbségét
+
+// 1. Kérj be 2 számot a felhasználótól és console-ra írasd ki a 2 szám szorzatát
+
+// 1. Kérj be egy életkort a felhasználótól és írasd ki a console-ra "Sziasztok, X éves vagyok"
+
+// 1. Kérj be egy életkort a felhasználótól és írasd ki felugró ablakba, hogy hány hónapos valójában
+
+// 1. Kérj be a felhasználótól 1 számot és egy szót, majd írasd ki a console-ra a bekért szám összegének a dupláját, majd írd mellé elválasztva szóközzel a szöveget.
---
+++ 2. változók/összetetebb-feladatok.md
@@ -0,0 +1,52 @@
+### 1. köszöntés
+
+Kérd be a felhasználótól a nevét és írj ki egy köszöntést helló alice alakban!
+
+tippek
+- `prompt`
+- `alert`
+- konkatenáció
+
+### 2. plusz egy
+
+Kérj be egy számot és írd ki az eggyel megnövelt értékét!
+
+tippek
+- `Number`
+
+### 3. hány szelet kenyér kell?
+
+Zsúrra készülünk. Kérd be, hogy hány vendég jön és hány szendvicset számoljunk
+vendégenként. Egy szendvics egy szelet kenyér. Írja ki a program a szükséges szeletek számát!
+
+### 4. bmi kalkulátor
+
+Írj programot, ami bekéri a BMI kiszámolásához szükséges adatokat és kiírja
+a BMIT-t!
+
+### 5. mérföld-kilométer konvertáló
+
+Írj programot, ami bekér egy mérföld értéket és kiírja a kilométer megfelelőjét!
+
+### 6. celsius-fahrenheit konvertáló
+
+Írj programot, ami bekér egy fahrenheit értéket és kiírja a celsius megfelelőjét!
+
+### 7. változók cseréje
+
+Kérj be egy-egy értéket két változóba! Cseréld meg a változók tartalmát!
+```js
+let a = prompt();   // pl. 42
+let b = prompt();   // pl. 55
+
+// ?
+
+a   // 55
+b   // 42
+```
+
+### 8. átutalás
+
+Kérd be, hogy mennyi pénz van Alice és Bob számláján! Írj programot, ami
+modellezi, hogy Alice pénzt küld Bobnak! Az utalandó összeget a felhasználótól
+kérd be!
